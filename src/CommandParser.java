f3fdb689aa9c106f097b1cb8322f3df2f993a2c6
koray <kyilmaz80@gmail.com>
2023-08-18 18:26:56 +0300

    matematiksel ifade kontrolü eklendi.

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -1,13 +1,47 @@
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class CommandParser {
     public static String parse(String command) {
         //TODO: lint tarzi sentaks kontrolu
-        //StringTokenizer i */+- icin sırayla recursive çalıştırarak islem yapılabili
+        //StringTokenizer i +- icin sırayla recursive çalıştırarak islem yapılabili
         //parse tree lere bakılabilir
-        return null;
+        StringTokenizer st = new StringTokenizer(command, "+-");
+        while(st.hasMoreElements()) {
+            if (!isTokenValid(st.nextToken())) {
+                return null;
+            }
+        }
+        return command;
     }
 
     public static int execute(String command) {
         //TODO: komutun parse edilmis halinin hesabi
         return 0;
+    }
+
+    private static boolean isTokenNumerical(String str) {
+        if (str.equals("") || str == null) {
+            return false;
+        }
+        try {
+            Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isTokenMathFunction(String str) {
+        String[] funcArray = str.split("\\(");
+        return Arrays.binarySearch(Constants.ALLOWED_MATH_FUNCTIONS, funcArray[0].toLowerCase()) >= 0;
+    }
+
+    private static boolean isTokenValid(String token) {
+        return isTokenExit(token) || isTokenNumerical(token) || isTokenMathFunction(token);
+    }
+
+    private static boolean isTokenExit(String token) {
+        return token.toLowerCase().equals(Constants.COMMAND_EXIT);
     }
 }
--- src/Constants.java
+++ src/Constants.java
@@ -1,3 +1,4 @@
 public class Constants {
         public static final String COMMAND_EXIT = "exit";
+        public static final String[] ALLOWED_MATH_FUNCTIONS = {"cos", "pow", "sin", "sqrt", "tan"};
 }
