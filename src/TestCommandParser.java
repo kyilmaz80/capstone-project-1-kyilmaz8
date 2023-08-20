1589226db7584f2e587e1f744493a85e61134f24
koray <kyilmaz80@gmail.com>
2023-08-20 21:12:26 +0300

    test kodu yeni testler

--- src/TestCommandParser.java
+++ src/TestCommandParser.java
@@ -1,10 +1,8 @@
 public class TestCommandParser {
     public static void main(String[] args) {
         assert CommandParser.parse("5+6*7").equals("567*+");
-        //String p2 = CommandParser.parse("5*6+7");
-        //System.out.println(p2);
         assert CommandParser.parse("5*6+7").equals("56*7+");
-        //String p = CommandParser.parse("5*6+7");
-        //System.out.println(p);
+        assert CommandParser.parse("5/4*3+2").equals("54/3*2+");
+        assert CommandParser.parse("5/4-3*2").equals("54/32*-");
     }
 }
