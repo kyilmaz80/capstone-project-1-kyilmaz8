5db55ad5aed3a7cec8b6a5a3bb98faf7c8a27056
koray <kyilmaz80@gmail.com>
2023-08-21 11:01:42 +0300

    test case'leri eklendi.

--- src/TestCommandParser.java
+++ src/TestCommandParser.java
@@ -4,5 +4,10 @@ public class TestCommandParser {
         assert CommandParser.parse("5*6+7").equals("56*7+");
         assert CommandParser.parse("5/4*3+2").equals("54/3*2+");
         assert CommandParser.parse("5/4-3*2").equals("54/32*-");
+        assert CommandParser.execute(CommandParser.parse("5+6*7")) == 47.0;
+        assert CommandParser.execute(CommandParser.parse("5*6+7")) == 37.0;
+        assert CommandParser.execute(CommandParser.parse("5/4*3+2")) == 5.75;
+        assert CommandParser.execute(CommandParser.parse("5/4-3*2")) == -4.75;
+
     }
 }
