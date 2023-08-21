32393bd5d4248b17186d774454c03dd630ee9cc8
koray <kyilmaz80@gmail.com>
2023-08-21 13:42:31 +0300

    basit aritmetik ifadeler yapilabilir hale geldi.

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -3,18 +3,36 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class CommandParser {
-    //private static Stack operatorsStack = new Stack();
-    //private static Stack operandsStack = new Stack();
     public static String parse(String command) {
         return convertToPostfixExpression(command);
     }
 
     public static double execute(String command) {
-        //TODO: komutun parse edilmis halinin hesabi
-        String op;
+        //TODO: komutun parse edilmis postfix halinin hesabi
+        String postfixExpression = command;
         double result = 0;
-
-        return result;
+        // read the expression from left to right
+        // push the element in to a stack if it is operand
+        // if the current character is an operator,
+        //   pop the two operands from the stack and
+        //   evaluate it
+        //   push back the result of the evaluation
+        // repeat until the end of the expression
+        Stack<Double> stack = new Stack<Double>();
+        String tokenString;
+        for(char tokenChar: postfixExpression.toCharArray()) {
+            tokenString = "" + tokenChar;
+            if (isTokenOperand(tokenString)) {
+                stack.push(Double.valueOf(tokenString));
+            } else {
+                // token is operator
+                Double val1 = stack.pop();
+                Double val2 = stack.pop();
+                result = doOperation(val1, val2, Operators.fromSymbol(tokenString));
+                stack.push(result);
+            }
+        }
+        return stack.pop();
     }
 
     private static String convertToPostfixExpression(String infixExpression) {
@@ -62,19 +80,14 @@ public class CommandParser {
 
         return sb.toString();
     }
-    private static double doOperation(double val1, double val2, String op) {
+    private static double doOperation(double val1, double val2, Operators op) {
         double result = -1;
-        if (op.equals("*")) {
-            result = val2 * val1;
-        } else if (op.equals("/")) {
-            result = val2 / val1;
-        } else if (op.equals("+")) {
-            result = val2 + val1;
-        } else if (op.equals("-")) {
-            result = val1 - val2;
-        } else {
-            //TODO: pass
-            System.out.println("NOT IMPLEMENTED!");
+        switch(op) {
+            case MULTIPLICATION -> result = val2 * val1;
+            case DIVISION -> result = val2 / val1;
+            case ADDITION -> result = val2 + val1;
+            case SUBTRACTION -> result = val2 - val1;
+            default -> System.out.println("NOT IMPLEMENTED!");
         }
         return result;
     }
@@ -114,5 +127,4 @@ public class CommandParser {
     private static boolean isTokenDelimiter(String token) {
         return Constants.DELIMITERS.indexOf(token) >= 0;
     }
-
 }
