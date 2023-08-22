507aa104dca5fdcd403e145cde935fe05095e64b
koray <kyilmaz80@gmail.com>
2023-08-22 18:19:39 +0300

    execute -> eval isim degisikligi

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
                 continue;
             }
             Menu.printOutput();
-            double result = CommandParser.execute(parsedCommand);
+            double result = CommandParser.eval(parsedCommand);
             System.out.println(result);
         } while (!command.equals(Constants.COMMAND_EXIT));
         Menu.exit();
