3a5295c15a3017ed1fe91cf9715ccb7c33a9d4e6
koray <kyilmaz80@gmail.com>
2023-08-25 00:44:30 +0300

    code refactor, whitespace bugfix

--- src/Constants.java
+++ src/Constants.java
@@ -3,4 +3,5 @@ public class Constants {
         public static final String[] ALLOWED_MATH_FUNCTIONS = {"cos", "pow", "sin", "sqrt", "tan"};
         public static final String DELIMITERS =  "+-*/(), ";
         public static final String WHITESPACE = " ";
+        public static final String BLANK = "";
 }
